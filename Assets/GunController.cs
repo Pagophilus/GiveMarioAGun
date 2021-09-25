587ec8b022d379ec730e6ed46ba2542560436168
gcarlson <grantcarlson6@gmail.com>
2021-09-24 22:45:21 -0400

    Added burst

--- Assets/GunController.cs
+++ Assets/GunController.cs
@@ -13,6 +13,8 @@ public class GunController : MonoBehaviour
     public GameObject impactFX;
 
     public int pellets = 5;
+    public int burstCount = 1;
+    public float burstGap = 0.25f;
     public float spread = 40.0f;
     public float recoil = 8.0f;
     public float fireRate = 4.0f;
@@ -35,12 +37,14 @@ public class GunController : MonoBehaviour
     }
 
 
-    void shoot()
+    IEnumerator shoot()
     {
-        gameObject.GetComponentInParent<Rigidbody2D>().AddForce(transform.up * -1.0f * recoil, ForceMode2D.Impulse);
+        //gameObject.GetComponentInParent<Rigidbody2D>().AddForce(transform.up * -1.0f * recoil, ForceMode2D.Impulse);
 
         if (hitscan)
         {
+            gameObject.GetComponentInParent<Rigidbody2D>().AddForce(transform.up * -1.0f * recoil, ForceMode2D.Impulse);
+
             // Declare a raycast hit to store information about what our raycast has hit
             RaycastHit2D hit;
 
@@ -79,28 +83,17 @@ public class GunController : MonoBehaviour
         {
             Vector2 fireAngle;
             GameObject o;
-            /*if (pellets != 1)
+
+            for (int h = 0; h < burstCount; h++)
             {
+                gameObject.GetComponentInParent<Rigidbody2D>().AddForce(transform.up * -1.0f * recoil, ForceMode2D.Impulse);
                 for (int i = 0; i < pellets; i++)
                 {
-                    fireAngle = Quaternion.Euler(0, 0, spread / 2.0f - i * spread / (pellets - 1)) * transform.up;
+                    fireAngle = Quaternion.Euler(0, 0, spread / 2.0f * (Random.value * 2 - 1)) * transform.up;
                     o = Instantiate(bullet, transform.position + transform.up * 0.5f, Quaternion.identity);
-                    o.GetComponent<Rigidbody2D>().AddForce(fireAngle * 20.0f, ForceMode2D.Impulse);
+                    o.GetComponent<Rigidbody2D>().AddForce(fireAngle * (18 + 4 * Random.value), ForceMode2D.Impulse);
                 }
-            }
-            else
-            {
-                fireAngle = transform.up;
-                o = Instantiate(bullet, transform.position + transform.up * 0.5f, Quaternion.identity);
-                o.GetComponent<Rigidbody2D>().AddForce(fireAngle * 20.0f, ForceMode2D.Impulse);
-
-            }*/
-
-            for (int i = 0; i < pellets; i++)
-            {
-                fireAngle = Quaternion.Euler(0, 0, spread / 2.0f * (Random.value * 2 - 1)) * transform.up;
-                o = Instantiate(bullet, transform.position + transform.up * 0.5f, Quaternion.identity);
-                o.GetComponent<Rigidbody2D>().AddForce(fireAngle * (18 + 4 * Random.value), ForceMode2D.Impulse);
+                yield return new WaitForSeconds(burstGap);
             }
         }
     }
@@ -113,7 +106,7 @@ public class GunController : MonoBehaviour
             if (Input.GetMouseButton(0))
             {
                 laserLine.enabled = true;
-                shoot();
+                StartCoroutine(shoot());
             } else
             {
                 laserLine.enabled = false;
@@ -121,7 +114,7 @@ public class GunController : MonoBehaviour
         }
         else if ((auto ? Input.GetMouseButton(0) : Input.GetMouseButtonDown(0)) && fireCountDown <= 0.0f)
         {
-            shoot();
+            StartCoroutine(shoot());
             fireCountDown = 1.0f / fireRate;
         }
         fireCountDown -= Time.deltaTime;
