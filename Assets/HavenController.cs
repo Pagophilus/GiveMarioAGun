ce66405b8b11f0b50bdc8c73b65640f1dc830827
gcarlson <grantcarlson6@gmail.com>
2021-10-06 00:12:50 -0400

    Added basic haven

---
+++ Assets/HavenController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static Platformer.Mechanics.BulletController;
+using static Platformer.Mechanics.PlayerController;
+using static Platformer.Mechanics.EnemyController;
+using static Platformer.Gameplay.EnemyDeath;
+using Platformer.Gameplay;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Mechanics
+{
+    public class HavenController : MonoBehaviour
+    {
+        public TimerController timer;
+
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log("ddd1 entering");
+            timer.pause();
+        }
+
+        void OnTriggerExit2D(Collider2D collision)
+        {
+            Debug.Log("ddd12 exiting");
+            timer.unpause();
+        }
+    }
+}
--- Assets/TimerController.cs
+++ Assets/TimerController.cs
@@ -11,6 +11,7 @@ namespace Platformer.Mechanics
         public float timeLeft = 100.0f;
         public GameObject player;
         private HUDController hud;
+        private bool timerRunning = true;
 
         // Start is called before the first frame update
         void Start()
@@ -18,16 +19,29 @@ namespace Platformer.Mechanics
             hud = GameObject.Find("Hud").GetComponent<HUDController>();
         }
 
+        public void pause()
+        {
+            timerRunning = false;
+        }
+
+        public void unpause()
+        {
+            timerRunning = true;
+        }
+
         // Update is called once per frame
         void Update()
         {
-            Damage(Time.deltaTime);
-            if (timeLeft <= 0)
+            if (timerRunning)
             {
-                player.GetComponent<PlayerController>().Die();
-                timeLeft = 100.0f;
+                Damage(Time.deltaTime);
+                if (timeLeft <= 0)
+                {
+                    player.GetComponent<PlayerController>().Die();
+                    timeLeft = 100.0f;
+                }
+                hud.UpdateTimer(timeLeft);
             }
-            hud.UpdateTimer(timeLeft);
         }
 
         public void Damage(float damage)
